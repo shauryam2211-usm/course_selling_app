aa72201781fe0375c18c74c2343c16cb86b00a1a
shauryam2211-usm <shauryam2211@gmail.com>
2026-06-15 01:41:58 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -8,9 +8,7 @@ const userRouter = Router();
 
 userRouter.post("/signup", async function(req, res) {
     const { email, password, firstName, lastName } = req.body; // TODO: adding zod validation
-    // TODO: hash the password so plaintext pw is not stored in the DB
-
-    // TODO: Put inside a try catch block
+  
     await userModel.create({
         email: email,
         password: password,
@@ -26,7 +24,7 @@ userRouter.post("/signup", async function(req, res) {
 userRouter.post("/signin",async function(req, res) {
     const { email, password } = req.body;
 
-    // TODO: ideally password should be hashed, and hence you cant compare the user provided password and the database password
+    
     const user = await userModel.findOne({
         email: email,
         password: password
